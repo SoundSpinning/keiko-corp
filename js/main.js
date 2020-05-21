007b5907880265cd260431713688009f2c006d7a
SoundSpinning <SoundSpinning@hotmail.com>
2020-05-21 14:22:25 +0100

    Performance 1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
 	//Navigation menu scrollTo
-	$('header nav ul li a').click(function(event){
-		event.preventDefault();
-		var section = $(this).attr('href');
-		var section_pos = $(section).position();
+	// $('header nav ul li a').click(function(event){
+	// 	event.preventDefault();
+	// 	var section = $(this).attr('href');
+	// 	var section_pos = $(section).position();
 
-		if(section_pos){
-			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
-		}
+	// 	if(section_pos){
+	// 		$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
+	// 	}
 		
-	});
+	// });
 
-	$('.app_link').click(function(e){
-		event.preventDefault();
-		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
-	});
+	// $('.app_link').click(function(e){
+	// 	event.preventDefault();
+	// 	$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
+	// });
 
 	//Show & Hide menu on mobile
 	$('.burger_icon').click(function(){
@@ -34,19 +34,19 @@ $(document).ready(function(){
 	wow.init();
 
 	//parallax effect initialization
-	$('.hero').parallax("50%", 0.3);
+	// $('.hero').parallax("50%", 0.3);
 
 	//Nice scroll initialization
-	$("html").niceScroll({
-		scrollspeed: 50,
-		autohidemode : false,
-		cursorwidth : 8,
-		cursorborderradius: 8,
-		cursorborder : "0",
-		background : "rgba(48, 48, 48, .4)",
-		cursorcolor : '#1f1f1f',
-		zindex : 999
-	});
+	// $("html").niceScroll({
+	// 	scrollspeed: 50,
+	// 	autohidemode : false,
+	// 	cursorwidth : 8,
+	// 	cursorborderradius: 8,
+	// 	cursorborder : "0",
+	// 	background : "rgba(48, 48, 48, .4)",
+	// 	cursorcolor : '#1f1f1f',
+	// 	zindex : 999
+	// });
 
 	//Testimonials slider initialization
 	$("#tslider").owlCarousel({
